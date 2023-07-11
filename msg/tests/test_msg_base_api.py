a396b198548fd48009934fbf48621782994776b4
Evgeny Namsaraev <enamsaraev13@icloud.com>
2023-07-11 23:45:36 +0300

    finish expense tests

--- msg/tests/test_msg_base_api.py
+++ msg/tests/test_msg_base_api.py
@@ -4,7 +4,7 @@ import json
 from django.urls import reverse
 from mixer.backend.django import mixer
 
-from dbcore.models import Expense, ExpenseCategoryProperty
+from dbcore.models import Expense, ExpenseCategory, ExpenseCategoryProperty
 
 
 def test_get_user_expense_category(db, api_login):
@@ -40,6 +40,30 @@ def test_creation_a_new_category_by_user(db, api_login):
 
     assert response.data['name'] == request_data['name']
     assert response.data['description'] == request_data['description']
+
+
+@pytest.mark.parametrize(
+        'name, description, status_code', [
+            ('', 123, 400),
+            (45, '', 400),
+            ('', '', 400),
+        ]
+)
+def test_creation_a_new_category_by_user_400(db, api_login, name, description, status_code):
+    api, user = api_login
+    category = mixer.blend('dbcore.ExpenseCategory')
+
+    request_data = {
+        "user_id": user.id,
+        "name": name,
+        "description": description
+    }
+    response = api.post(reverse('msg:create_new_user_category'), data=request_data)
+
+    last_category = ExpenseCategory.objects.last()
+
+    assert response.status_code == status_code
+    assert last_category.id == category.id
 
 
 def test_creation_new_user_expense(db, api_login):
@@ -98,18 +122,26 @@ def test_creating_a_full_user_expense(db, api_login):
     api, user = api_login
 
     category = mixer.blend('dbcore.ExpenseCategory', name='some_name', user=user)
-    expense = mixer.blend('dbcore.Expense', user=user, category=category)
     request_data = {
         "user_id": user.id,
         "expense_category_name": category.name,
-        "name": "тест трата",
-        "expense": 273.5,
+        "name": "тест трата полная",
+        "expense": 456.9,
         "description": "-",
-        "raw_string": "тест трата 273.5"
+        "raw_string": "тест трата полная 456.9"
     }
 
     response = api.generic(method='POST', path=reverse('msg:create_full_expense'), data=json.dumps(request_data), content_type='application/json')
-    expense = Expense.objects.filter(user__id=user.id, category__name=category.name).last()
-    expense_property = ExpenseCategoryProperty.objects.filter(category__name=category.name, category__expenses__id=expense.id)
+    created_expense = Expense.objects.filter(user__id=user.id, category__name=category.name).last()
+    created_expense_property = ExpenseCategoryProperty.objects.filter(category__name=category.name, category__expenses__id=created_expense.id).last()
 
     assert response.status_code == 201 
+
+    assert created_expense.user.id == user.id
+    assert created_expense.category.id == category.id
+
+    assert created_expense_property.category.id == category.id
+    assert created_expense_property.name == request_data['name']
+    assert created_expense_property.expense == request_data['expense']
+    assert created_expense_property.description == request_data['description']
+    assert created_expense_property.raw_string == request_data['raw_string']
