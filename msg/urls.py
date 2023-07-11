336706e939c40f46080c9f8e98ec5b1c2815c7ff
enamsaraev <86794710+enamsaraev@users.noreply.github.com>
2023-07-11 16:27:17 +0300

    expense creation api (#3)

--- msg/urls.py
+++ msg/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
 
-from msg.views import GetUserExpenseCategories, CreateNewUserExpenseCategory, CreateNewExpense
+from msg.views import (
+    GetUserExpenseCategories, CreateNewUserExpenseCategory, 
+    CreateNewExpense, CreateNewExpenseProps, CreateFullExpense
+)
 
 
 urlpatterns = [
     path('categories/', GetUserExpenseCategories.as_view(), name='get_user_expense_categories'),
     path('create_category/', CreateNewUserExpenseCategory.as_view(), name='create_new_user_category'),
-    path('create_expense/', CreateNewExpense.as_view(), name='new_expense_creation')
+    path('create_expense/', CreateNewExpense.as_view(), name='new_expense_creation'),
+    path('create_expense_props/', CreateNewExpenseProps.as_view(), name='new_expense_category_property_cration'),
+    path('create_full_expense/', CreateFullExpense.as_view(), name='create_full_expense'),
 ]
 
 
